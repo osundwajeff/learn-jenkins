4e28bf7902d3b1ac4d27ec849a1e07b03e6598ed
Jeff Osundwa <jeff@kartoza.com>
2024-03-12 22:06:31 +0300

    updates

--- playwright/production-tests/base-url.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Setting BASE_URL for test site"
-BASE_URL=https://kartoza.com
--- playwright/run-all-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for dir in $(find "$(pwd)" -type d -name '*_test'); do
-    if [ -f "$dir/package.json" ]; then
-        echo "Running tests in $dir"
-        cd $dir
-        npx playwright test
-    fi
-done
